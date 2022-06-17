fc72f71891d3fb57bf363a8c41cd45fa15f070b0
Namat Bayamanov <namataxvd15@gmail.com>
2022-06-17 21:33:29 +0600

    connected with FireBase an ArticleList

--- src/components/ArticleCreate/ArticleCreate.js
+++ src/components/ArticleCreate/ArticleCreate.js
@@ -1,8 +1,31 @@
+import axios from "axios";
+
 function ArticleCreate() {
-  return ( 
-    <>
-      
-    </>
+
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    axios.post(`https://practice-axios-default-rtdb.firebaseio.com/articles.json`, Object.fromEntries(formData.entries()))
+  } 
+
+  return (
+    <div>
+      <form onSubmit={onSubmitForm}>
+        <div>
+          <input type="text" name="title" />
+        </div>
+
+        <div>
+          <textarea name="description" />
+        </div>
+        <button>
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
 
